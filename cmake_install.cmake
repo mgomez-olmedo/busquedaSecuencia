634c3a786bb1c35167e6b4f0bd4c99329667ecb8
Manuel Gomez-Olmedo <mgomez@decsai.ugr.es>
2022-03-08 09:28:51 +0100

    Update cmake_install.cmake

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/gte/mgomez/ownCloud/desarrollo/infdiag/busquedaSecuencia/busquedaSecuencia/test/cmake_install.cmake")
+  include("/home/gte/mgomez/ownCloud/desarrollo/infdiag/codigoGithub/busquedaSecuencia/test/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
